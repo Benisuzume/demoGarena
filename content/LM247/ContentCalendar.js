9cd926708031bbfe069ef65c986ec431bc4cb6f5
Cuong Gosu <cuonggosu@Cuongs-iMac.local>
2018-10-23 22:11:26 +0700

    mac-23-10-2018

--- content/LM247/ContentCalendar.js
+++ content/LM247/ContentCalendar.js
@@ -1,106 +1,91 @@
 import React, { Component } from "react";
 import {
     View, Text, Image,
-    SectionList
+    SectionList, StyleSheet
 } from "react-native";
 import dataFlatListCalendar from "../../data/dataFlatListCalendar";
 
 class SectionListItem extends Component {
     render() {
         return (
-            <View style={{
-                flexDirection: 'row',
-                height: 110,
-                justifyContent: 'space-between',
-                paddingHorizontal: 10,
-                alignItems: 'center',
-                borderTopWidth: 1,
-                borderColor: '#F5F5F5'
-            }}>
-                <View style={{
-                    flex: 1,
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                }}>
-                    <Image
-                        source={this.props.item.logoTeam1}
-                        style={{
-                            width: 32, height: 32,
-                            resizeMode: 'contain',
-                            marginRight: 5
-                        }}
-                    />
-                    <Text style={{
-                        fontSize: 18,
-                        color: 'black'
-                    }}
-                        numberOfLines={1}>
+            <View style={styles.Container}>
+                {/* Start */}
+                <View style={styles.Start}>
+                    <Image source={this.props.item.logoTeam1} style={styles.FixImg} />
+                    <Text numberOfLines={1}
+                        style={{ color: 'black', paddingLeft: 5, fontSize: 22 }}>
                         {this.props.item.nameTeam1}
                     </Text>
                 </View>
-                <View style={{
-                    flex: 1,
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                    marginHorizontal: 20
-                }}>
-                    <Text style={{
-                        fontSize: 25,
-                        color: 'black'
-                    }}>
+
+                {/* center */}
+                <View style={styles.Center}>
+                    <Text style={{ color: 'black', fontSize: 25 }}>
                         {this.props.item.number1}
                     </Text>
-                    <View style={{
-                        height: 20,
-                        justifyContent: 'center',
-                        borderRadius: 10,
-                        borderWidth: 1,
-                        borderColor: '#F5F5F5',
-                        flex: 1,
-                        marginHorizontal: 5
-                    }}>
-                        <Text style={{
-                            fontSize: 14,
-                            color: 'black',
-                        }}
-                            numberOfLines={1}>
+                    <View style={styles.Result}>
+                        <Text numberOfLines={1} style={{ color: 'black', paddingHorizontal: 3 }}>
                             {this.props.item.status}
                         </Text>
                     </View>
-                    <Text style={{
-                        fontSize: 25,
-                        color: 'black'
-                    }}>
+                    <Text style={{ color: 'black', fontSize: 25 }}>
                         {this.props.item.number2}
                     </Text>
                 </View>
-                <View style={{
-                    flex: 1,
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    justifyContent: 'flex-end',
-                }}>
-                    <Text style={{
-                        fontSize: 18,
-                        color: 'black'
-                    }}
-                        numberOfLines={1}>
-                        {this.props.item.nameTeam2}
+
+                {/* End */}
+                <View style={styles.End}>
+                    <Text numberOfLines={1}
+                        style={{ color: 'black', paddingRight: 5, fontSize: 22 }}>
+                        {this.props.item.nameTeam1}
                     </Text>
-                    <Image
-                        source={this.props.item.logoTeam2}
-                        style={{
-                            width: 32, height: 32,
-                            resizeMode: 'contain',
-                            marginRight: 5
-                        }}
-                    />
+                    <Image source={this.props.item.logoTeam2} style={styles.FixImg} />
                 </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    Container: {
+        flex: 1,
+        flexDirection: 'row',
+        height: 110,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        borderColor: '#F5F5F5'
+    },
+    Start: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    FixImg: {
+        width: 32,
+        height: 32,
+        resizeMode: 'contain',
+    },
+    Center: {
+        flex: 2,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        // backgroundColor: 'red'
+    },
+    Result: {
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: 'grey',
+        marginHorizontal: 10,
+    },
+    End: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+});
 
 class SectionHeader extends Component {
     render() {
